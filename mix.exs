e7ff9527b3bcc15bb2dff657b6e05bc300f4ccc2
Tom Clarke <tom@u2i.com>
2026-03-21 15:32:03 -0400

    chore: bump to 0.2.11

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule SandboxCase.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/pinetops/sandbox_case"
-  @version "0.2.10"
+  @version "0.2.11"
 
   def project do
     [
